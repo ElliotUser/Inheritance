5b5de482caaa198af9fe28d93cdc9629133c21c9
ElliotUser <andersen.e@bk.ru>
2018-07-20 22:50:44 +0300

    Initial commit

---
+++ src/exc01/Box.java
@@ -0,0 +1,33 @@
+package exc01;
+
+public class Box {
+    public double width;
+    public double height;
+    public double depth;
+
+    public Box(Box ob){
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    public Box(double width,double height, double depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public Box(){
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    public Box(double len){
+        width=height=depth=len;
+    }
+
+    double volume(){
+        return width * height * depth;
+    }
+}
---
+++ src/exc01/BoxWeight.java
@@ -0,0 +1,14 @@
+package exc01;
+
+public class BoxWeight extends Box {
+    public double weight;
+
+    BoxWeight(double w, double h, double d, double m){
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
+    }
+
+
+}
